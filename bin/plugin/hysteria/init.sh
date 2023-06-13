c04931f5eb3cbaec0d2ca932c3274d2b918b7cd9
Eken Chan <maskedeken@yahoo.com>
2023-06-13 16:46:29 +0800

    fix hysteria build error

--- bin/plugin/hysteria/init.sh
+++ bin/plugin/hysteria/init.sh
@@ -9,5 +9,5 @@ CURR="plugin/hysteria"
 CURR_PATH="$PROJECT/$CURR"
 
 git submodule update --init "$CURR/*"
-cd $CURR_PATH/src/main/go/hysteria/app
+cd $CURR_PATH/src/main/go/hysteria
 go mod download -x
