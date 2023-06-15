2b7e2af6c2fbdc60673a0a3de702603991ecd31c
Eken Chan <maskedeken@yahoo.com>
2023-06-15 17:29:21 +0800

    add hysteria-v2 plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,7 @@ when {
         include(":plugin:brook")
         include(":plugin:trojan-go")
         include(":plugin:hysteria")
+        include(":plugin:hysteria-v2")
         include(":plugin:mieru")
         include(":plugin:tuic-v5")
     }
